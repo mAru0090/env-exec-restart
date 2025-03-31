cc86f9200dfbfe91232c1afabe9dd986f2953936
mAru <maru1015928@gmail.com>
2025-04-01 06:25:32 +0900

    Updat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use clap::{Parser,Subcommand};
+use anyhow::Result as R;
+
+/**
+	== usage ==
+	env-exec-restart.exe [OPTIONS] --env-file <ENV_FILE>
+**/
+
+
+#[derive(Debug,Parser)]
+struct Args {
+	#[arg(short,long)]
+	env_file:String,
+	#[arg(short,long)]
+	all:bool,
+	#[arg(short,long)]
+	list:bool,
+}
+fn main() -> R<()> {
+    Args::parse();
     println!("Hello, world!");
+    Ok(())
 }
